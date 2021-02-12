eae7b1df0a1323d21b613d2c79adb58023414f27
Fawmain <dicleacet@outlook.com>
2021-02-12 15:17:35 +0200

    add Form

--- CoronaVirus.py
+++ CoronaVirus.py
@@ -2,6 +2,7 @@ from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QApplication, QMainWindow
 import sys
 import pandas as pd
+import Form 
 
 df = pd.read_csv("worldometer_data.csv")
 result = df.head(10)
