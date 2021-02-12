eaf461ff7d498f814fc1b6049a6fb3468d0cf9a1
Fawmain <dicleacet@outlook.com>
2021-02-12 19:40:59 +0200

    add country

--- CoronaVirus.py
+++ CoronaVirus.py
@@ -2,8 +2,26 @@ from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QApplication, QMainWindow
 import sys
 import pandas as pd
-import Form 
+from Form import Ui_MainWindow
 
-df = pd.read_csv("worldometer_data.csv")
-result = df.head(10)
-print(result)
+class App(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(App, self).__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.df = pd.read_csv("worldometer_data.csv")
+
+    def Country(self):
+        selam = self.df["Country/Region"]
+        print(selam)
+        for i in selam:
+            self.ui.comboBox.addItem(i)
+            
+def myAPP():
+    myAPP = QtWidgets.QApplication(sys.argv)
+    win = App()
+    win.Country()
+    win.show()
+    sys.exit(myAPP.exec_())
+
+myAPP()
